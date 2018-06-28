1842a6de75f8d5bd680610ebdce3735537f98477
Vathsal Veena Shashidar <picasiovs@gmail.com>
2018-06-28 12:54:07 -0700

    PB_Lecture-47

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+        ProcessInput();
+	}
+
+    void ProcessInput() {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            print("Thrusting");
+            if (!audioSource.isPlaying){
+                audioSource.Play();
+            }
+  
+        }else {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward * Time.deltaTime);
+            print("Rotating Left");
+        }else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward * Time.deltaTime);
+            print("Rotating Right");
+        }
+
+
+    }
+}
